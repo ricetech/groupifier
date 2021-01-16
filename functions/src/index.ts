63f71a889714cb64bbce4fac60e2fedcf2db5ea6
Jonathan Papineau <public@jpapineau.ca>
2021-01-16 01:59:00 -0500

    Implement the basics to connect to the database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,21 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
+import * as pg from "pg-promise";
+import {config} from "./databasesecret";
+
+const db = pg()(config);
+
+// eslint-disable-next-line require-jsdoc
+async function test() {
+  const users = await db.any("SELECT * FROM Groups");
+  console.log(users);
+}
+
+test();
+
+// export const test = functions.https.onRequest((request, response) => {
+//     database.
 // });
