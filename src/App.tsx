363c5c10d8d2b4441080a47b083836b4fd670a5e
Janakitti <janakittiratana@gmail.com>
2021-01-16 11:25:17 -0500

    Create pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import { HomePage } from './pages/HomePage/HomePage';
+import { DashboardPage } from './pages/DashboardPage/DashboardPage';
+import { GroupPage } from './pages/GroupPage/GroupPage';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path='/dashboard'>
+            <DashboardPage />
+          </Route>
+          <Route path='/group'>
+            <GroupPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
