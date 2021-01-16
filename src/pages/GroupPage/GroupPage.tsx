0274ec7ce1e4f248f9dd8db37b2d5a5bf23fdda7
Sun971 <aisorya.bhandari@gmail.com>
2021-01-16 15:46:38 -0500

    Adding in Group Page

--- src/pages/GroupPage/GroupPage.tsx
+++ src/pages/GroupPage/GroupPage.tsx
@@ -1,3 +1,20 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export const GroupPage = () => <div>GROUP</div>;
+import { Sidebar } from '../../components/Sidebar/Sidebar';
+import { TeamCard } from '../../components/TeamCard/TeamCard';
+
+export const GroupPage = () => (
+  <Row className='full-row'>
+    <Col xs={2}>
+      <Sidebar name='Bob' />
+    </Col>
+    <Col xs={10}>
+      <TeamCard
+        teamCardOption='Dream Team'
+      />
+    </Col>
+  </Row>
+);
