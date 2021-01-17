55d7f7386446e80d489bb1d51119bb17482381b0
Eric C <39015447+ricetech@users.noreply.github.com>
2021-01-17 07:41:02 -0500

    Add exclamation mark

--- src/pages/GroupBuilderPage/GroupBuilderSuccessPage/GroupBuilderSuccessPage.tsx
+++ src/pages/GroupBuilderPage/GroupBuilderSuccessPage/GroupBuilderSuccessPage.tsx
@@ -4,8 +4,8 @@ import Col from 'react-bootstrap/Col';
 
 export const GroupBuilderSuccessPage = () => (
   <Row className='full-row justify-content-md-center'>
-    <Col xs={4} className='align-self-center'>
-      <h1> An email of your group will be sent shortly</h1>
+    <Col xs={12} md={8} lg={4} className='align-self-center'>
+      <h1>An email of your group will be sent shortly!</h1>
     </Col>
   </Row>
 );
