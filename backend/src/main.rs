d2afdbf5768032a12899f0def97cb927229d2331
Jonathan Papineau <public@jpapineau.ca>
2021-04-29 17:55:13 -0400

    Add the PostegreSQL Diesel driver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel;
+
+#[database("postgres_db")]
+struct DbConn(diesel::PgConnection);
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +14,8 @@ fn index() -> &'static str {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(DbConn::fairing())
+        .launch();
 }
