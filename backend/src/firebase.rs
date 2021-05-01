5e3b1959597b3e22be09a6bbc26391e21078def3
Jonathan Papineau <public@jpapineau.ca>
2021-05-01 11:49:54 -0400

    Use proper error handling for the Firebase API

--- backend/src/firebase.rs
+++ backend/src/firebase.rs
@@ -1,20 +1,39 @@
-use std::ffi::{CStr};
+use std::error::Error;
+use std::ffi::CStr;
+use std::fmt;
+use std::fmt::Formatter;
 use std::os::raw::c_char;
+use std::str::Utf8Error;
 
 extern "C" {
     fn initialize() -> *const c_char;
 }
 
-unsafe fn convert_to_str<'a>(char_ptr: *const c_char) -> &'a str {
-    let c_str = CStr::from_ptr(char_ptr);
-    return c_str.to_str().expect("Error converting c_str to Rust string");
+#[derive(Debug)]
+pub struct FirebaseError<'a> {
+    message: &'a str,
 }
 
-pub fn initialize_firebase() {
+impl Error for FirebaseError<'_> {}
+
+impl fmt::Display for FirebaseError<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+unsafe fn convert_to_str<'a>(char_ptr: *const c_char) -> Result<&'a str, Utf8Error> {
+    let c_str = CStr::from_ptr(char_ptr);
+    return c_str.to_str();
+}
+
+pub fn initialize_firebase<'a>() -> Result<(), Box<dyn Error>> {
     let status_ptr = unsafe { initialize() };
     let status = unsafe { convert_to_str(status_ptr) };
 
-    if status != "" {
-        panic!("Error Initializing Firebase: {}", status)
+    match status {
+        Err(e) => Err(Box::new(e)),
+        Ok("") => Ok(()),
+        Ok(e) => Err(Box::new(FirebaseError{message: e})),
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 use rocket_contrib::databases::diesel;
+
+mod firebase;
 
 #[database("postgres_db")]
 struct DbConn(diesel::PgConnection);
@@ -14,6 +18,11 @@ fn index() -> &'static str {
 }
 
 fn main() {
+    match firebase::initialize_firebase() {
+        Ok(_) => { println!("Successfully initialized Firebase API") }
+        Err(e) => { panic!("Could not initialize Firebase API: {}", e.to_string()) }
+    }
+
     rocket::ignite()
         .mount("/", routes![index])
         .attach(DbConn::fairing())
