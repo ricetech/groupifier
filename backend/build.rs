d2878609425be24beb3ec71e0ceccf9da1a1cfce
Jonathan Papineau <public@jpapineau.ca>
2021-05-01 13:06:30 -0400

    Force the Rust build to fail if the Go build fails

--- backend/build.rs
+++ backend/build.rs
@@ -1,17 +1,23 @@
 use std::env;
 use std::process::Command;
 
-// Compile and link the go library to access the Firebase API
+// Compile and link the Go library to access the Firebase API
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     // First compile the library
-    let output = Command::new("./firebaseConnector/build.sh")
+    let go_output = Command::new("./firebaseConnector/build.sh")
         .arg(&out_dir)
         .output()
-        .expect("Could not build Go Firebase library");
+        .unwrap();
 
-    println!("{}", String::from_utf8_lossy(&output.stderr));
+    println!("{}", String::from_utf8_lossy(&go_output.stdout));
+    println!("{}", String::from_utf8_lossy(&go_output.stderr));
+
+    // Fail the build if the Go library didn't compile
+    if !go_output.status.success() {
+        panic!("Go Firebase library failed to compile: {}", String::from_utf8_lossy(&go_output.stderr))
+    }
 
     // Statically link it to the rust project
     let lib = "firebase";
